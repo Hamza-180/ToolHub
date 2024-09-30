a3f7a263bc54f05776ef5260ff0f74b54da4bd8e
Hamza <hamza.amghar@student.ehb.be>
2024-09-30 23:43:16 +0200

    Begin Project toolhub > firstly testing

---
+++ src/main/java/be/ehb/toolhub/ToolHubApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.toolhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/ehb/toolhub/ToolHubApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.toolhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
